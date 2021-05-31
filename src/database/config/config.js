a9102ac1ee1673a7394419162440ee2dac966173
Ismael-Boff-UTN <ismael.boff@alumnos.frm.utn.edu.ar>
2021-05-30 22:30:31 -0300

    editedurl

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -4,20 +4,20 @@ module.exports = {
     "password": 'd424461f',
     "database": "heroku_92d25e5ecab2ca2",
     "host": "us-cdbr-east-04.cleardb.com",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "test": {
     "username": "root",
     "password": null,
     "database": "database_test",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
     "username": "root",
     "password": null,
     "database": "database_production",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   }
 }
