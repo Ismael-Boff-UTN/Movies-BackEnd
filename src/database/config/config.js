8a39d2cdcf2f44c97388c20e82fddee15c4445be
Ismael-Boff-UTN <ismael.boff@alumnos.frm.utn.edu.ar>
2021-05-30 22:51:19 -0300

    asda

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -7,17 +7,17 @@ module.exports = {
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": "b3e2cf4870eb34",
+    "password": 'd424461f',
+    "database": "heroku_92d25e5ecab2ca2",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "b3e2cf4870eb34",
+    "password": 'd424461f',
+    "database": "heroku_92d25e5ecab2ca2",
+    "host": "us-cdbr-east-04.cleardb.com",
     "dialect": "mysql"
   }
 }
